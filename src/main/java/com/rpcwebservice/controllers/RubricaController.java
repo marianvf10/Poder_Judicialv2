8129e68c89a9bb8b55d68464b05dbb379393fec3
mariano <mariano.vergara@est.fi.uncoma.edu.ar>
2023-04-06 19:00:56 -0300

    few changes

--- src/main/java/com/rpcwebservice/controllers/RubricaController.java
+++ src/main/java/com/rpcwebservice/controllers/RubricaController.java
@@ -18,7 +18,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @CrossOrigin(origins = "http://localhost:8081")
-@Tag(name = "Rubrica", description = "API de Gestion de Rubricas")
+@Tag(name = "Rubrica", description = "Web Service de Gestion de Rubricas")
 @RestController
 @RequestMapping("/api")
 public class RubricaController {
@@ -28,7 +28,7 @@ public class RubricaController {
     @Operation(
             summary = "Recuperar rubrica por medio de id de sociedad",
             description = "Obtener un objeto rubrica por medio del id de una sociedad. La respuesta es un objeto Rubrica con numero apertura, numero clausura, ru denominacion,id de sociedad y fecha apertura",
-            tags = { "rubricas", "get" })
+            tags = { "rubricas"})
     @ApiResponses({
             @ApiResponse(responseCode = "200", content = { @Content(schema = @Schema(implementation = RubricaDTO.class), mediaType = "application/json") }),
             @ApiResponse(responseCode = "404", description = "No se encontro una sociedad con el id ingresado",content = { @Content(schema = @Schema()) }),
