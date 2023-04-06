89c2a3b0b4d80458de3a6ff2bae8a1d97d782521
mariano <mariano.vergara@est.fi.uncoma.edu.ar>
2023-04-06 18:47:04 -0300

    Se agrego la documentacion de swagger para el web service de rubrica

--- src/main/java/com/rpcwebservice/controllers/RubricaController.java
+++ src/main/java/com/rpcwebservice/controllers/RubricaController.java
@@ -1,7 +1,14 @@
 package com.rpcwebservice.controllers;
 
 import com.rpcwebservice.dtos.RubricaDTO;
+import com.rpcwebservice.entities.Rubrica;
 import com.rpcwebservice.services.RubricaService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.repository.query.Param;
 import org.springframework.http.HttpStatus;
@@ -11,12 +18,22 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @CrossOrigin(origins = "http://localhost:8081")
+@Tag(name = "Rubrica", description = "API de Gestion de Rubricas")
 @RestController
 @RequestMapping("/api")
 public class RubricaController {
 
     @Autowired
     private RubricaService rubricaService;
+    @Operation(
+            summary = "Recuperar rubrica por medio de id de sociedad",
+            description = "Obtener un objeto rubrica por medio del id de una sociedad. La respuesta es un objeto Rubrica con numero apertura, numero clausura, ru denominacion,id de sociedad y fecha apertura",
+            tags = { "rubricas", "get" })
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", content = { @Content(schema = @Schema(implementation = RubricaDTO.class), mediaType = "application/json") }),
+            @ApiResponse(responseCode = "404", description = "No se encontro una sociedad con el id ingresado",content = { @Content(schema = @Schema()) }),
+            @ApiResponse(responseCode = "500", description = "Error interno del servidor",content = { @Content(schema = @Schema()) })
+    })
     @GetMapping("/sociedad_rubricas/{id}")
     public ResponseEntity<List<RubricaDTO>> getRubricasConSociedad(@PathVariable("id") Integer id){
         List<RubricaDTO> rubricasDTOS = rubricaService.getSociedadRubricasById(id);
--- src/main/java/com/rpcwebservice/dtos/RubricaDTO.java
+++ src/main/java/com/rpcwebservice/dtos/RubricaDTO.java
@@ -2,13 +2,15 @@ package com.rpcwebservice.dtos;
 
 import org.springframework.beans.factory.annotation.Value;
 
+import java.util.Date;
+
 public interface RubricaDTO {
     String getNumero_apertura();
 
     String getNumero_clausura();
 
     @Value("#{target.apertura}")
-    String getFecha_apertura();
+    Date getFecha_apertura();
 
     @Value("#{target.denominacion}")
     String getRu_Denominacion();
