c1398120df588e011dcc08de24d5f22ce70e6f7c
mariano <mariano.vergara@est.fi.uncoma.edu.ar>
2023-04-11 20:21:25 -0300

    Comentada algunas lineas que no se usan

--- src/main/java/com/rpcwebservice/controllers/RubricaController.java
+++ src/main/java/com/rpcwebservice/controllers/RubricaController.java
@@ -1,8 +1,8 @@
 package com.rpcwebservice.controllers;
 
 import com.rpcwebservice.dtos.RubricaDTO;
-import com.rpcwebservice.dtos.RubricasDT;
-import com.rpcwebservice.entities.Rubrica;
+//import com.rpcwebservice.dtos.RubricasDT;
+//import com.rpcwebservice.entities.Rubrica;
 import com.rpcwebservice.services.RubricaService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
@@ -11,7 +11,7 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.repository.query.Param;
+//import org.springframework.data.repository.query.Param;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -29,7 +29,7 @@ public class RubricaController {
     @Operation(
             summary = "Recuperar rubrica por medio de id de sociedad",
             description = "Obtener un objeto rubrica por medio del id de una sociedad. La respuesta es un objeto Rubrica con numero apertura, numero clausura, ru denominacion,id de sociedad y fecha apertura",
-            tags = { "rubricas"})
+            tags = { "Rubrica"})
     @ApiResponses({
             @ApiResponse(responseCode = "200", content = { @Content(schema = @Schema(implementation = RubricaDTO.class), mediaType = "application/json") }),
             @ApiResponse(responseCode = "404", description = "No se encontro una sociedad con el id ingresado",content = { @Content(schema = @Schema()) }),
