9158f735d34913c3bd4e45626be6db3a76c9fc39
mariano <mariano.vergara@est.fi.uncoma.edu.ar>
2023-04-17 11:52:42 -0300

    Se agrego la documentacion del service de antecedente

--- src/main/java/com/rpcwebservice/controllers/AntecedenteController.java
+++ src/main/java/com/rpcwebservice/controllers/AntecedenteController.java
@@ -1,8 +1,15 @@
 package com.rpcwebservice.controllers;
 
 import com.rpcwebservice.dtos.AntecedenteDTO;
+import com.rpcwebservice.dtos.RubricaDTO;
 import com.rpcwebservice.exceptions.ResourceNotFoundException;
 import com.rpcwebservice.services.AntecedenteServiceImp;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,12 +19,22 @@ import java.util.List;
 
 @CrossOrigin(origins = "http://localhost:8081")
 @RestController
+@Tag(name = "Antecedente", description = "Web Service de Gestion de Antecedentes")
 @RequestMapping("/api")
 public class AntecedenteController {
 
     @Autowired
     private AntecedenteServiceImp antecedenteServiceImp;
 
+    @Operation(
+            summary = "Recuperar antecedente por medio del cuit de sociedad",
+            description = "Obtener un objeto antecedente por medio del cuit de una sociedad. La respuesta es un objeto Antecedente con fecha,descripcion,nombre como tipo de antecedente y descripcion",
+            tags = { "Antecedente"})
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", content = { @Content(schema = @Schema(implementation = RubricaDTO.class), mediaType = "application/json") }),
+            @ApiResponse(responseCode = "404", description = "No se encontro una sociedad con el cuit ingresado",content = { @Content(schema = @Schema()) }),
+            @ApiResponse(responseCode = "500", description = "Error interno del servidor",content = { @Content(schema = @Schema()) })
+    })
     @GetMapping("/sociedad_antecedentes/{cuit}")
     public ResponseEntity<?> getSociedadAntecedenteByCuit(@PathVariable("cuit") String cuit) {
         List<AntecedenteDTO> antecedentes;
