9fb1909380a71d624662f889857f09f5f0687f38
mariano <mariano.vergara@est.fi.uncoma.edu.ar>
2023-04-20 10:53:18 -0300

    Documentacion agregada al endpoint de getSocio

--- src/main/java/com/rpcwebservice/controllers/SocioController.java
+++ src/main/java/com/rpcwebservice/controllers/SocioController.java
@@ -3,6 +3,11 @@ package com.rpcwebservice.controllers;
 import com.rpcwebservice.dtos.SocioDTO;
 import com.rpcwebservice.exceptions.ResourceNotFoundException;
 import com.rpcwebservice.services.SociosService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -17,6 +22,15 @@ public class SocioController {
 
     @Autowired
     private SociosService sociosService;
+    @Operation(
+            summary = "Recuperar socio por medio del cuit de sociedad",
+            description = "Obtener un objeto socio por medio del cuit de una sociedad. La respuesta es un objeto Socio con nombre,apellido,sexo,numero_documento,denominacion,participacion,idsociedad y deno",
+            tags = { "Socio"})
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", content = { @Content(schema = @Schema(implementation = SocioDTO.class), mediaType = "application/json") }),
+            @ApiResponse(responseCode = "404", description = "No se encontro una sociedad con el cuit provisto",content = { @Content(schema = @Schema()) }),
+            @ApiResponse(responseCode = "500", description = "Error interno del servidor",content = { @Content(schema = @Schema()) })
+    })
     @GetMapping("/sociedad_socios/{cuit}")
     public ResponseEntity<?> getSocioBySociedadCuit(@PathVariable("cuit")String cuit){
         List<SocioDTO> socios;
