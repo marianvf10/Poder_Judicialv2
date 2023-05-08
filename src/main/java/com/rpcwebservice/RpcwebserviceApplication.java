90918f86867a088fc7c166bae37ce886e159bb8d
mariano <mariano.vergara@est.fi.uncoma.edu.ar>
2023-05-08 18:53:56 -0300

    Configuracion global de cors añadida

--- src/main/java/com/rpcwebservice/RpcwebserviceApplication.java
+++ src/main/java/com/rpcwebservice/RpcwebserviceApplication.java
@@ -2,12 +2,31 @@ package com.rpcwebservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class RpcwebserviceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RpcwebserviceApplication.class, args);
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")
+						.allowedMethods("*")
+						.allowedOrigins("http://localhost:8080")
+						.allowedHeaders("")
+						.allowCredentials(false)
+						.maxAge(-1);
+
+			}
+		};
 	}
 
 }
