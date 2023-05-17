13d60315347a23a4ce00eacd7b84de32c30ef752
mariano <mariano.vergara@est.fi.uncoma.edu.ar>
2023-05-17 10:11:47 -0300

    Se agrego el tag que faltaba

--- src/main/java/com/rpcwebservice/controllers/InscripcionController.java
+++ src/main/java/com/rpcwebservice/controllers/InscripcionController.java
@@ -9,6 +9,7 @@ import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -16,6 +17,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api")
+@Tag(name = "Inscripcion", description = "Web Service de Gestion de Inscripciones")
 public class InscripcionController {
 
     @Autowired
